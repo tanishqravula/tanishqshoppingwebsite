0f3073fd6a3e18d33956292fdda47ee5513cbeaa
tanishqravula <121391761+tanishqravula@users.noreply.github.com>
2023-08-07 02:37:24 +0530

    Add files via upload

---
+++ src/database/index.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+const configOptions = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
+
+const connectToDB = async () => {
+  const connectionUrl =
+    "mongodb+srv://sangammukherjee2022:123456782023@cluster0.t1aiqty.mongodb.net/";
+
+  mongoose
+    .connect(connectionUrl, configOptions)
+    .then(() => console.log("Ecommerce database connected successfully!"))
+    .catch((err) =>
+      console.log(`Getting Error from DB connection ${err.message}`)
+    );
+};
+
+export default connectToDB;
